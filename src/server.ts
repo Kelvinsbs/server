17981ebb2ce008f51f8bee184de3d9fa3c2f19b7
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-01-24 21:20:58 -0300

    ambiente node criado

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,12 @@
 
  const app = express();
 
-//  www.minhaapi.com/ads 
  app.get('/ads', (request, response) => {
-    // console.log("Acessou ads!");
     return response.json([
         {id: 1, name: "anuncio 1"},
         {id: 2, name: "anuncio 2"},
         {id: 3, name: "anuncio 3"},
+        {id: 4, name: "anuncio 4"},
     ]);
  })
 
